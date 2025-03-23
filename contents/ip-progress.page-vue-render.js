7aeac0fd00e37125df309b7170a968d02f7818ff
damithc <1673303+damithc@users.noreply.github.com>
2025-03-23 13:59:25 +0800

    Site Update. [skip ci]

--- contents/ip-progress.page-vue-render.js
+++ contents/ip-progress.page-vue-render.js
@@ -8,7 +8,7 @@ with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"fixed":""}
 with(this){return _c('h1',{staticClass:"display-4",attrs:{"id":"ip-progress-dashboard"}},[_v("iP progress dashboard"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#ip-progress-dashboard","onclick":"event.stopPropagation()"}})])}
 },function anonymous(
 ) {
-with(this){return _c('p',[_c('span',{staticClass:"dimmed"},[_v("[This page was "),_c('mark',[_v("last updated on "),_c('strong',[_v("2025-03-21 @14:08")])]),_v("]")])])}
+with(this){return _c('p',[_c('span',{staticClass:"dimmed"},[_v("[This page was "),_c('mark',[_v("last updated on "),_c('strong',[_v("2025-03-23 @12:56")])]),_v("]")])])}
 },function anonymous(
 ) {
 with(this){return _c('td',[_c('span',{staticClass:"badge bg-danger me-1"},[_v("!"),_c('s',[_v("3")])]),_c('span',{staticClass:"badge bg-success me-1"},[_v("4")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("5")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("6")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("7")])])}
@@ -1748,6 +1748,6 @@ with(this){return _c('td',[_c('span',{staticClass:"badge bg-success me-1"},[_v("
 with(this){return _c('td',[_c('span',{staticClass:"badge bg-success me-1"},[_v("Forking")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("Tracker")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("PR Creation")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("branch-Level-5")]),_c('span',{staticClass:"badge bg-info me-1"},[_v("branch-A-Packages")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("Week 6 Jar")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("branch-Level-6")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("branch-Level-7")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("branch-A-JavaDoc")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("branch-Level-9")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("Merging PRs")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("Published UG")]),_c('span',{staticClass:"badge bg-success me-1"},[_v("JAR released")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Sun, 23 Mar 2025, 12:13:00 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.6.0")]),_v(", generated on Sun, 23 Mar 2025, 13:59:17 GMT+8]")])])])}
 }];
   
